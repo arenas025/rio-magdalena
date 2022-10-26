20aaec56e6a0a325a90d49c2f11c97cc6808d116
sergio arenas <sergioarenas025@gmail.com>
2022-10-26 08:28:29 -0500

    feat: ServerSideRenderer

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,17 +3,16 @@ import Layout from '@components/Layout/Layout'
 import KawaiiHeader from '@components/KawaiiHeader/KawaiiHeader'
 import ProductList from '@components/ProductList/ProductList'
 
-const HomePage = () => {
-  const [productList, setProductList] = useState<TProduct[]>([])
-
-  useEffect(() => {
-    window
-      .fetch('/api/avo')
-      .then((response) => response.json())
-      .then(({ data }: TAPIAvoResponse) => {
-        setProductList(data)
-      })
-  }, [])
+export const getServerSideProps = async () => {
+  const response = await fetch('http://localhost:3000/api/avo')
+  const {data : productList} : TAPIAvoResponse = await response.json()
+  return {
+    props:{
+      productList
+    }
+  }
+}
+const HomePage = ({productList}:{productList:TProduct[]}) => {
 
   return (
     <Layout>
